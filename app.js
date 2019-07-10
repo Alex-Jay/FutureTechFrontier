fc44b1752db1ac3de02980b7ecd8501e2cc6b345
Alex Jay <alexjaycontact@gmail.com>
2019-07-10 18:36:28 +0100

    Add App.js & Users.js
    App.js is the default application running using NodeJS. Users.js will
    handle registrations and logging in.

---
+++ app.js
@@ -0,0 +1,34 @@
+const express       = require('express');
+const path          = require('path');
+const bodyParser    = require('body-parser');
+const cors          = require('cors');
+const passport      = require('passport');
+const mongoose      = require('mongoose');
+
+/* Express Application */
+const app = express();
+
+/* Require Users Endpoint */
+const users = require('./routes/users');
+
+/* Port */
+const port = 3000;
+
+/* CORS Middleware */
+app.use(cors());
+
+/* Body Parser Middleware */
+app.use(bodyParser.json());
+
+/* Users Endpoint */
+app.use('/users', users);
+
+/* Index Route */
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+/* Start Server */
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+});
