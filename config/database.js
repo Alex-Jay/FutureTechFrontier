bebbd6cebcd92e20b8372098d489bcb17d5e7c79
Alex Jay <alexjaycontact@gmail.com>
2019-07-11 18:41:21 +0100

    Further attempts of fixing connection string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports =
 {
-    database: 'mongodb+srv://admin:Alexcom199%26%26@cluster0-xs87a.mongodb.net/meanauthapp',
+    database: 'mongodb+srv://admin_:Alexcom199%26%26@cluster0-xs87a.mongodb.net/meanauthapp',
     secret: 'yoursecret'
 }
