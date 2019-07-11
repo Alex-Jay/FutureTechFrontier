163dd319a71999750ce1534e729e0e9307b667b5
Alex Jay <alexjaycontact@gmail.com>
2019-07-11 18:30:23 +0100

    Attempting to fix MongoDB integration

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports =
 {
-    database: 'mongodb+srv://admin:Alexcom199%2A%26%26@cluster0-xs87a.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb//admin:Alexcom199%2A%26%26@cluster0-xs87a.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
