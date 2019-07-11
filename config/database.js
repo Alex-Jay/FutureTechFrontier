f042b03d48fa7b290c82d1d22c0963566e08e586
Alex Jay <alexjaycontact@gmail.com>
2019-07-11 20:54:37 +0100

    Fix Heroku > MongoDB connection errors

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports =
 {
-    database: 'mongodb://admin:***REMOVED***@ds243607.mlab.com:43607/heroku_msb8s9c0',
+    database: 'mongodb://alex:alex123@ds243607.mlab.com:43607/heroku_msb8s9c0',
     secret: 'yoursecret'
 }
